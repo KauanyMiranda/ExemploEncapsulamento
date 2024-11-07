46efee876631eed38e8e0acc81b99efe93f4fb4e
Kauany Miranda <kauany.m.dantas@gmail.com>
2024-11-06 22:26:32 -0400

    Inicio do código

---
+++ Conta.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+using static System.Convert;
+public class Conta
+{
+    private double _saldo;
+
+    public void SetSaldo(double s)
+    {
+        if (s >= 0 )
+        {
+            _saldo = s;
+        }
+        else
+        {
+            WriteLine("Saldo Inválido");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Convert;
+using static System.Console;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Conta c = new Conta();
+
+        Write("Digite o saldo: ");
+        double saldo = ToDouble(ReadLine());
+        c.SetSaldo(saldo);
+    } 
+}
